1c2c35975ed9e7297d964257988b2e1b37142c46
soham <sohamjm13@gmail.com>
2025-06-17 20:02:09 +0530

    new begining

--- Hello.c
+++ Hello.c
@@ -2,12 +2,5 @@
 
 int main()
 {
-    printf("Har Har Mahadev");
-    printf("Om namah shivay ha ha shmbhu");
-    printf("Om namah shivay namo narayan");
-    printf("JAY JAY SOMNATH");
-    printf("OM NAMO NARAYANA");
-    printf("JAY JAY MAHAKAL");
-    printf("JAY MALLIKARJUN");
     return 0;
 }
