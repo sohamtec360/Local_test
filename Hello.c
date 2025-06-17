bf9dd16d80191b07b66ee7bba0e3d3461f3e564c
soham <sohamjm13@gmail.com>
2025-06-17 19:33:26 +0530

    new om namo narayana line is added

--- Hello.c
+++ Hello.c
@@ -5,5 +5,6 @@ int main()
     printf("Har Har Mahadev");
     printf("Om namah shivay ha ha shmbhu");
     printf("Om namah shivay namo narayan");
+    printf("OM NAMO NARAYANA");
     return 0;
 }
